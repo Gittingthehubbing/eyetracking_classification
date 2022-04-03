2414b0f7e940441f196d91b78890121a0983bf0d
Gittingthehubbing <pvwork13@gmail.com>
2022-04-03 22:24:28 +0100

    update

--- config.py
+++ config.py
@@ -7,30 +7,30 @@ config_dict = {
     "plot_histograms": True,
     "plot_learning_curves":True,
     "save_learning_curves":True,
-    "max_seq_length": 600,
-    "every_nth_entry": 20,#28 works well for BERT with max 50 seq. 40 # up to 40 seems to work for BERT
+    "max_seq_length": 20,
+    "every_nth_entry": 20,
     "use_padding": True,
     "normalise_x_y_within_sample": False,
     "one_hot_y": False,
     "batch_size": 64,
-    "num_epochs": 125,
+    "num_epochs": 200,
     "use_reduced_set": False ,# reduces number of samples used
-    "model_to_use": ["BERT","LSTM"][1],
-    "n_layers_BERT": 8, #2 for BERT
+    "model_to_use": ["BERT","LSTM"][0],
+    "n_layers_BERT": 8, 
     "head_multiplication_factor": 16,
     "num_attention_heads": 2,
     "hidden_dim_lstm":256,
     "n_layers_LSTM":1,
-    "lr": 2.5e-5,
-    "lr_LSTM": 1.832e-05,
+    "lr": 8e-5,
+    "lr_LSTM": 1e-04,
     "lr_scheduling": ["const","ExponentialLR","multistep","anneal"][1],
     "lr_sched_exp_fac": 0.99,
     "multistep_milestones": [25,50,75,100],
     "gamma_multistep": 0.5,
     "min_lr_anneal": 1e-6,
-    "redo_samples": True, #if loop requires chages to dataset
-    "loop_var_name": "max_seq_length",
-    "loop_vals" : np.arange(50,600,50),
+    "redo_samples": True, #if loop requires changes to dataset
+    "loop_var_name": "__",
+    "loop_vals" : [1],#np.arange(25,200,25),
     "loop_plot_xscale":["linear","log"][0]
 }
 
